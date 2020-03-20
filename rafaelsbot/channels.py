a792ee1a70506620323e21a8e29037b724ea7f1d
rafaelurben <rafael.urben@gmail.com>
2020-03-20 12:12:31 +0100

    Benutzerkanäle automatisch löschen
    Benutzerkanäle werden nun automatisch gelöscht

--- rafaelsbot/channels.py
+++ rafaelsbot/channels.py
@@ -7,6 +7,17 @@ class Channels(commands.Cog):
         self.bot = bot
         self.color = 0xee00ff
 
+        @bot.event
+        async def on_voice_state_update(member, before, after):
+            if before.channel and before.channel.category and before.channel.category.name.upper() == "BENUTZERKANÄLE" and before.channel.members == []:
+                await before.channel.delete(reason="War leer")
+                channelowner = utils.get(before.channel.guild.members, name=before.channel.name.split("#")[0], discriminator=before.channel.name.split("#")[1])
+                EMBED = Embed(title="Sprachkanal gelöscht!", color=self.color)
+                EMBED.set_footer(text=f'Kanal von {member.name}',icon_url=member.avatar_url)
+                EMBED.add_field(name="Server",value=member.guild.name)
+                await channelowner.send(embed=EMBED)
+
+
     @commands.bot_has_permissions(manage_channels = True)
     @commands.command(
         brief='Erstelle deinen Textkanal',
@@ -24,7 +35,7 @@ class Channels(commands.Cog):
             overwrites = { ctx.guild.default_role: PermissionOverwrite(read_messages=False), ctx.author: PermissionOverwrite(read_messages=True,send_messages=True) }
             newchannel = await category.create_text_channel(name=(ctx.author.name+"-"+ctx.author.discriminator),overwrites=overwrites)
             EMBED = Embed(title="Textkanal erstellt!", color=self.color)
-            EMBED.set_footer(text=f'Angefordert von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
+            EMBED.set_footer(text=f'Kanal von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
             EMBED.add_field(name="Kanal",value="<#"+str(newchannel.id)+">")
             await ctx.send(embed=EMBED)
         else:
@@ -44,7 +55,7 @@ class Channels(commands.Cog):
         if channel:
             await channel.delete(reason="Vom Benutzer gelöscht")
             EMBED = Embed(title="Textkanal gelöscht!", color=self.color)
-            EMBED.set_footer(text=f'Angefordert von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
+            EMBED.set_footer(text=f'Kanal von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
             EMBED.add_field(name="Server",value=ctx.guild.name)
             await ctx.author.send(embed=EMBED)
         else:
@@ -69,10 +80,10 @@ class Channels(commands.Cog):
                 await ctx.author.edit(voice_channel=channel,reason="Benutzer hat den Kanal erstellt")
             raise commands.BadArgument(message="Du hast bereits einen Sprachkanal!")
         elif category:
-            overwrites = { ctx.guild.default_role: PermissionOverwrite(connect=False,speak=False), ctx.author: PermissionOverwrite(connect=True,speak=True) }
+            overwrites = { ctx.guild.default_role: PermissionOverwrite(connect=False,speak=True,read_messages=False), ctx.author: PermissionOverwrite(connect=True,speak=True,read_messages=True) }
             newchannel = await category.create_voice_channel(name=(ctx.author.name+"#"+ctx.author.discriminator),overwrites=overwrites)
             EMBED = Embed(title="Sprachkanal erstellt!", color=self.color)
-            EMBED.set_footer(text=f'Angefordert von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
+            EMBED.set_footer(text=f'Kanal von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
             await ctx.send(embed=EMBED)
             if ctx.author.voice:
                 await ctx.author.edit(voice_channel=newchannel,reason="Benutzer hat den Sprachkanal erstellt")
@@ -93,7 +104,7 @@ class Channels(commands.Cog):
         if channel:
             await channel.delete(reason="Vom Benutzer gelöscht")
             EMBED = Embed(title="Sprachkanal gelöscht!", color=self.color)
-            EMBED.set_footer(text=f'Angefordert von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
+            EMBED.set_footer(text=f'Kanal von {ctx.message.author.name}',icon_url=ctx.author.avatar_url)
             EMBED.add_field(name="Server",value=ctx.guild.name)
             await ctx.send(embed=EMBED)
         else:
